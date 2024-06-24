b9d657a161b5f365c33bbf0cc458c037cce38398
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-06-24 16:19:44 +0200

    Modified Orchestraion Function 1 as needed for the new class

--- Orchestration/Function1.cs
+++ Orchestration/Function1.cs
@@ -3,6 +3,8 @@ using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.DurableTask;
 using Microsoft.DurableTask.Client;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Orchestration
 {
@@ -13,15 +15,17 @@ namespace Orchestration
             [OrchestrationTrigger] TaskOrchestrationContext context)
         {
             ILogger logger = context.CreateReplaySafeLogger(nameof(Function1));
-            logger.LogInformation("Saying hello.");
+            logger.LogInformation("Starting orchestration.");
             var outputs = new List<string>();
 
-            // Replace name and input with values relevant for your Durable Functions Activity
+            // Calling existing activity functions
             outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Tokyo"));
             outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Seattle"));
             outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "London"));
 
-            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+            // Calling the new activity function
+            outputs.Add(await context.CallActivityAsync<string>(nameof(PerformTask), "SampleTask"));
+
             return outputs;
         }
 
@@ -33,6 +37,16 @@ namespace Orchestration
             return $"Hello {name}!";
         }
 
+        [Function(nameof(PerformTask))]
+        public static string PerformTask([ActivityTrigger] string taskName, FunctionContext executionContext)
+        {
+            ILogger logger = executionContext.GetLogger("PerformTask");
+            logger.LogInformation($"Performing task: {taskName}");
+
+            // Simulate task execution
+            return $"Task {taskName} completed.";
+        }
+
         [Function("Function1_HttpStart")]
         public static async Task<HttpResponseData> HttpStart(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
@@ -42,10 +56,9 @@ namespace Orchestration
             ILogger logger = executionContext.GetLogger("Function1_HttpStart");
 
             // Function input comes from the request content.
-            string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(
-                nameof(Function1));
+            string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(nameof(Function1));
 
-            logger.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
+            logger.LogInformation("Started orchestration with ID = '{instanceId}'.");
 
             // Returns an HTTP 202 response with an instance management payload.
             // See https://learn.microsoft.com/azure/azure-functions/durable/durable-functions-http-api#start-orchestration
