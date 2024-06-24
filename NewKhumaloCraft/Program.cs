33c77364e88f2cf06a0d4fe791d25d19a87db50a
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-06-24 15:57:39 +0200

    Addded the orchestra durable function to the main program

--- NewKhumaloCraft/Program.cs
+++ NewKhumaloCraft/Program.cs
@@ -8,11 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("NewDBCnn")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NewDBCnn")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddDefaultTokenProviders()
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddDefaultTokenProviders()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Register IHttpClientFactory
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
