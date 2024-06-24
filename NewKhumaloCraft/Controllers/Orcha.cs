152f8374902e8d21e292a38141bb5ae0cb0e7204
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-06-24 15:54:48 +0200

    Created required durable function and made to new controller to work with it

---
+++ NewKhumaloCraft/Controllers/Orcha.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewKhumaloCraft.Controllers
+{
+    public class OrchaController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public OrchaController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StartOrchestration()
+        {
+            var client = _clientFactory.CreateClient();
+            var response = await client.PostAsync("https://<your-function-app-name>.azurewebsites.net/api/Function1_HttpStart", null);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Content($"Durable Function started. Status response: {content}");
+            }
+
+            return Content("Failed to start Durable Function");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
